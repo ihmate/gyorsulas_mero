b6659240814207bc7842fee4a916bc0469d819b7
ihmate <ihaszm.mate@gmail.com>
2022-08-24 10:08:50 +0200

    horizontális és vertikális működik

--- mukodo_program/grafikon_proba_horizontal.py
+++ mukodo_program/grafikon_proba_horizontal.py
@@ -12,19 +12,19 @@ smd_config = SharedMemoryDict(name='config', size=1024)
 #name of the .txt file
 file_name = "proba_adat3.txt"
 
-list = [0, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250]
+speed_list = [0, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250]
 
 #font style for labels
 title_font = {'family':'serif','color':'black','size':20}
 label_font = {'family':'serif','color':'darkred','size':15}
 
 #number of x and y-points -- refresh rate in ms
-ws_ylim = 25
-ylim = 50
-xlim = 250
+ylim = 250
+xlim = 50
+ws_xlim = 26
 ms = 100
-wspd_idx = 24
-tspd_idx = 24
+wspd_idx = 25
+tspd_idx = 25
 
 #changing plot color depending on the speed difference
 def plot_color():
@@ -40,7 +40,8 @@ def plot_color():
         line_color = "red"
 
     plt.title(f"{wheel_speed[wspd_idx]:.0f} KM/H (Wheel Speed)", fontdict = title_font, loc='right', c = line_color)
-    ax.plot(wheel_speed, wheel_speed_ypoints, linewidth = '7', color = line_color) 
+    ax.plot(wheel_speed_ypoints, wheel_speed, linewidth = '7', color = line_color) 
+
 
 # function to update the data
 def my_function(i):
@@ -55,22 +56,22 @@ def my_function(i):
     ax.cla()
 
     # plot target_speed
-    ax.plot(target_speed, target_speed_ypoints, linewidth = '30')
+    ax.plot(target_speed_ypoints, target_speed, linewidth = '30')
 
     ax.set_ylim(0, ylim)
     ax.set_xlim(0, xlim)
 
     # Set ticks frequency, position and text -- empty on y
-    ax.set_xticks(list, list)
-    ax.set_yticks([0, 5, 15, 25, 35, 45, 50], ["-2,5s", "-2s", "-1s", "0s", "+1s", "+2s", "-2,5s"])
-    ax.xaxis.tick_top()
-    ax.tick_params(axis='x', colors='darkred', size = 10)
+    ax.set_yticks(speed_list, speed_list)
+    ax.set_xticks([0, 5, 15, 25, 35, 45, 50], ["-2,5s", "-2s", "-1s", "0s", "+1s", "+2s", "-2,5s"])
+    ax.yaxis.tick_right()
+    ax.tick_params(axis='y', colors='darkred', size = 10)
     
     #set title and label texts
     plot_color()
 
-    plt.xlabel("Speed", fontdict = label_font)
-    plt.ylabel(f"{ms} ms", fontdict = label_font)
+    plt.xlabel(f"{ms} ms", fontdict = label_font)
+    plt.ylabel("Speed", fontdict = label_font)
 
     plt.grid(color = 'black', linestyle = '--', linewidth = 1)
 
@@ -80,11 +81,11 @@ if __name__ == '__main__':
     f = open(file_name, "r")
 
     # start collections with zeros and make ypoints for all xpoints
-    wheel_speed = collections.deque(np.zeros(ws_ylim))
-    wheel_speed_ypoints = range(0, ws_ylim)
+    wheel_speed = collections.deque(np.zeros(ws_xlim))
+    wheel_speed_ypoints = range(0, ws_xlim)
 
-    target_speed = collections.deque(np.zeros(ylim))
-    target_speed_ypoints = range(0, ylim)
+    target_speed = collections.deque(np.zeros(xlim))
+    target_speed_ypoints = range(0, xlim)
 
     # define and adjust figure - bgcolor
     fig = plt.figure(figsize=(12,6), facecolor='#DEDEDE')
