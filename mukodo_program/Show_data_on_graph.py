faf91b619d66191bbb6a0fc7a42d82f512b1acde
ihmate <ihaszm.mate@gmail.com>
2023-02-13 15:04:10 +0100

    összes függvény 1 fájlban

--- mukodo_program/Show_data_on_graph.py
+++ mukodo_program/Show_data_on_graph.py
@@ -6,8 +6,14 @@ import time
 import sys 
 from matplotlib.animation import FuncAnimation
 from multiprocessing import Process
-import gyorsulas_menu as menu
-import Can_data_reader
+#import gyorsulas_menu as menu
+#import Can_data_reader
+import shutil
+from canlib import canlib
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *  
+import os
 
 #number of x and y-points -- refresh rate in ms -- error margin
 error_margin = 4                #színváltáshoz
@@ -17,6 +23,114 @@ scan_idx = wheel_speed_x_limit - 1
 x_lim = wheel_speed_x_limit * 4
 futas_darab_szam = 0
 shared_value = multiprocessing.Value('d', 0.0)  #processek között megosztott változó
+
+
+#can adatok kiolvasásához 2 függvény
+def printframe(frame, width, shared_value):
+    factor = 0.0075
+    
+    if(frame.id == 178):
+        msg = bytearray(frame.data)
+
+        front_right_wheel = (msg[7] << 8) | msg[6]
+        front_right_wheel *= factor
+
+        front_left_wheel = (msg[5] << 8) | msg[4]
+        front_left_wheel *= factor
+
+        avarage_speed = (front_right_wheel + front_left_wheel) / 2
+        print("Avarage speed: ", avarage_speed)
+        shared_value.value = avarage_speed
+
+
+def monitor_channel(shared_value):
+    ticktime = 0
+    ch = canlib.openChannel(0, bitrate = canlib.Bitrate.BITRATE_500K, flags=canlib.canOPEN_ACCEPT_VIRTUAL)
+    ch.setBusOutputControl(canlib.canDRIVER_NORMAL)
+    ch.busOn()
+
+    width, height = shutil.get_terminal_size((80, 20))
+
+    timeout = 0.5
+    tick_countup = 0
+    if ticktime <= 0:
+        ticktime = None
+    elif ticktime < timeout:
+        timeout = ticktime
+
+    print("Listening...")
+    while True:
+        try:
+            frame = ch.read(timeout=int(timeout * 1000))
+            printframe(frame, width, shared_value)
+        except canlib.CanNoMsg:
+            if ticktime is not None:
+                tick_countup += timeout
+                while tick_countup > ticktime:
+                    print("tick")
+                    tick_countup -= ticktime
+        except KeyboardInterrupt:
+            print("Stop.")
+            break
+
+    ch.busOff()
+    ch.close()
+
+
+#menühöz 5 darab függvény
+def getFolder():
+   path = "./wltp_fajlok/"
+   dir_list  = os.listdir(path)
+   return dir_list 
+
+def wltp_files(window, dir_list):
+   tk.Label(window, text="WLTP fájlok:").grid(row = 0, column = 0, sticky = 'w')
+
+   wltp_file = tk.StringVar()
+   wltp_input = ttk.Combobox(window, width = 70, values = dir_list, state = "readonly", textvariable = wltp_file)
+   wltp_input.grid(row = 0, column = 1, sticky = 'w')
+
+   return wltp_file
+
+def Log_fajl(window):
+   tk.Label(window, text="Log fájl neve:").grid(row = 1, column = 0, sticky = 'w')
+
+   LOG_fajl_nev = tk.StringVar()
+   LOG_input = tk.Entry(window, width = 73, textvariable = LOG_fajl_nev)
+   LOG_input.grid(row = 1, column = 1, sticky = 'w')
+
+   return LOG_fajl_nev
+
+
+def mintavetelezesi_gyakorisag(window):
+   tk.Label(window, text="Mintavetelezési gyakoriság(ms):").grid(row = 2, column = 0, sticky = 'w')
+
+   Mintavetelezesi_gyakorisag = tk.StringVar()
+
+   Timing_input = tk.Spinbox(window, width = 71, from_=100, to=300, textvariable = Mintavetelezesi_gyakorisag)
+   Timing_input.grid(row = 2, column = 1, sticky = 'w')
+
+   return Mintavetelezesi_gyakorisag
+
+
+def start(): 
+   # creating window and setting attributes
+   window = tk.Tk()
+   window.state('zoomed')
+   window.title("WLTP mérés")
+
+   #creating components
+   wltp_fajl = wltp_files(window, getFolder())
+   LOG_fajl_nev = Log_fajl(window)
+   Mintavetelezesi_gyakorisag = mintavetelezesi_gyakorisag(window)
+
+   # Create Start button
+   Button(window, text = 'Program indítása!', bd = '5', command = window.destroy).grid(row = 3, column = 0, columnspan = 2, sticky = 'n') 
+
+   window.mainloop()
+
+   return(("./wltp_fajlok/" + wltp_fajl.get()), LOG_fajl_nev.get(), int(Mintavetelezesi_gyakorisag.get()))
+
 
 def lista_feltoltes(ms):
     #tengelyek listáinak létrehozása
@@ -118,12 +232,13 @@ def my_function(i, kezdes_ido, speed_list, time_tick_list, time_number_list):
 if __name__ == '__main__':
     multiprocessing.freeze_support()
     #processként a beolvasás futtatása || megosztott változó átadásával
-    p1 = Process(target=Can_data_reader.monitor_channel, args=(shared_value, ))
+    #p1 = Process(target=Can_data_reader.monitor_channel, args=(shared_value, ))
+    p1 = Process(target=monitor_channel, args=(shared_value, ))
     p1.daemon = True #azért, hogy leálljon a programmal együtt a process
     p1.start()
 
     #paraméterek kiolvasása a menüből (log fájl neve , késleltetés ideje) 
-    data_file_name, log_file_name, ms = menu.start()    
+    data_file_name, log_file_name, ms = start()    
     read_data_file = open(data_file_name, "r")  #open file for reading
 
     # start collections with zeros and make xpoints for all ypoints
